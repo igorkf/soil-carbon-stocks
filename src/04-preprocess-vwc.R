9e07e9542338add60ad24f62b64b0e1b7c712424
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2023-10-13 21:27:11 -0500

    fix vwc-vs-wvc typo

--- src/04-preprocess-vwc.R
+++ src/04-preprocess-vwc.R
@@ -3,52 +3,46 @@
 # https://github.com/leombastos/2023_aghack_vwc/blob/main/code/02%20visualization.qmd
 ##################################################################################
 
-# install if needed
-if(!require(USAboundaries)){
-  remotes::install_github("ropensci/USAboundaries")
-  install.packages("USAboundariesData", repos = "https://ropensci.r-universe.dev", type = "source")
-}
-
+library(ggplot2)
 library(sf)
-library(USAboundaries)
 library(terra)
 
 # read all rasters
 rasters <- list.files(path = "output", pattern = ".tif$", all.files = T, full.names = T)
-# df_rasters <- data.frame(path = rasters)
-# df_rasters$start_date <- as.Date(substr(df$path, 36, 45), "%Y.%m.%d")
-# df_rasters$end_date <- as.Date(substr(df$path, 47, 56), "%Y.%m.%d")
 
-# read points of interest and intersect with WVC rasters
-df_points <- read.csv("output/points_soc.csv")
-df_points$id <- 1:nrow(df_points)
+# read points of interest and intersect with VWC
+df_points <- read.csv("output/points.csv")
 ps <- vect(df_points[, c("lon", "lat")], crs = "epsg:4326")
 
 # read raster, project, and intersect with points of interest
-df_wvc <- data.frame()
+df_vwc <- data.frame()
 for (path in rasters) {
   r <- rast(path)
   r <- project(r, "epsg:4326")
   df_temp <- extract(r, ps)
-  colnames(df_temp) <- c("id", "wvc")
+  colnames(df_temp) <- c("id", "vwc")
   df_temp$path <- path
-  df_temp <- cbind(df_points, df_temp[, c("wvc", "path")])  # bind points dataset
-  df_wvc <- rbind(df_wvc, df_temp)
+  df_temp <- cbind(df_points, df_temp[, c("vwc", "path")])  # bind points dataset
+  df_vwc <- rbind(df_vwc, df_temp)
   cat("raster:", path, "\n")
 }
 
-# plot sequence of WVC across time for some points
-plot(wvc ~ 1, data = df_wvc[df_wvc$id == 1, ], type = "b")
-plot(wvc ~ 1, data = df_wvc[df_wvc$id == 100, ], type = "b")
-plot(wvc ~ 1, data = df_wvc[df_wvc$id == 200, ], type = "b")
-plot(wvc ~ 1, data = df_wvc[df_wvc$id == 300, ], type = "b")
-plot(wvc ~ 1, data = df_wvc[df_wvc$id == 400, ], type = "b")
+# order by point id
+df_vwc <- df_vwc[order(df_vwc$id), ]
+rownames(df_vwc) <- NULL
 
-# write WVC to file
-write.csv(df_wvc, "output/points_soc_wvc.csv", row.names = F)
+# create index from path
+df_vwc$index <- sapply(strsplit(df_vwc$path, split = "_"), `[`, 3)
+df_vwc$index <- as.integer(df_vwc$index)
+df_vwc$id <- factor(df_vwc$id)
 
-# TODO: check ways to do aggregation in WVC as it is a time-series data
-# aggregate by point id
-df_agg <- aggregate(cbind(wvc, stock) ~ id, data = df_wvc, FUN = median)
-cor(df_agg$wvc, df_agg$stock)
-plot(df_agg$wvc, df_agg$stock)
+# plot VWC time series for each county
+ggplot(df_vwc, aes(x = index, y = vwc)) +
+  geom_line(aes(group = id), linewidth = 0.2, alpha = 0.5) +
+  facet_wrap(~county) + 
+  labs(x = 'Week number', y = 'VWC', 
+       title = 'Volumetric Water Content of 20 sampled points from Jan to Set (2023)') +
+  theme_bw()
+
+# write vwc to file
+write.csv(df_vwc, "output/vwc.csv", row.names = F)
