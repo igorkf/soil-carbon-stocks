727ff952a2bb70de7bfcaacfd4883433f98c143b
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2023-10-18 18:46:43 -0500

    fix all calculations in merging data

--- src/06-merge-data.R
+++ src/06-merge-data.R
@@ -11,7 +11,6 @@ soc <- soc[, c("id", "fraction", "stock")]
 
 # precipitation
 pr <- read.csv("output/precipitation.csv")
-pr$year <- NULL
 
 # evapotranspiration
 et <- read.csv("output/Evapotranspiration.csv")
@@ -26,7 +25,7 @@ et <- et %>%
   drop_na()
 
 # linear interpolation on et
-n <- as.integer(as.Date("2023-08-31") - as.Date("2023-01-01"))
+n <- as.integer(as.Date("2023-08-31") - as.Date("2023-01-01")) + 1
 et2 <- data.frame()
 for (id in unique(et$id)) {
   et_sub <- et[et$id == id, ]
@@ -50,70 +49,73 @@ hz <- hz[, c("id", "mukey", "chkey", "dbovendry_r", "om_r", "oc")]
 # add texture on horizons
 query <- sprintf("SELECT chkey, texture, texdesc FROM chtexturegrp WHERE chkey IN %s", format_SQL_in_statement(unique(hz$chkey)))
 texture <- SDA_query(query)
+fc <- readxl::read_excel("data/texture_fc.xlsx")
 
 # volumetric water content
 vwc <- read.csv("output/vwc.csv")
-vwc <- vwc %>% 
-  mutate(start = as.Date(str_sub(path, 36, 45), format = "%Y.%m.%d")) %>% 
-  mutate(month = month(start)) %>% 
-  group_by(id, month) %>%
-  summarise(vwc = mean(vwc))
+vwc$date <- as.Date(gsub("\\.", "\\-", substr(vwc$path, 27, 36)))
+vwc$mo <- strftime(vwc$date, "%m")
+vwc$yr <- strftime(vwc$date, "%Y")
+vwc <- vwc[, c("id", "vwc", "day", "date", "mo", "yr")]
 
 # joining
-data <- left_join(et, pr, by = c("id", "month")) %>% 
-  left_join(vwc, by = c("id", "month")) %>% 
+data <- left_join(et2, vwc, by = c("id", "day")) %>%
+  left_join(pr, by = c("id", "day")) %>%
   left_join(hz, by = "id") %>% 
   left_join(soc, by = "id") %>% 
-  left_join(texture, by = "chkey")
+  left_join(texture, by = "chkey") %>% 
+  left_join(fc, by = "texture")
 
 # keep first texture because of mismatch
 data <- data %>% 
-  group_by(id, month) %>% 
+  group_by(id, day) %>% 
   slice(1)
 
 # dropping NA
-data <- data[!is.na(data$evapotranspiration), ]
+colSums(is.na(data))
 data <- data[!is.na(data$vwc), ]
 data <- data[!is.na(data$stock), ]
 colSums(is.na(data))
 
 # number of points per county?
-table(filter(data, month == 1)$county)
+table(filter(data, day == 1)$county)
 
 # creating new variables
 # data$evapotranspiration <- data$evapotranspiration * 0.1
-data$vwc_L <- ((data$vwc / data$dbovendry_r) * (data$dbovendry_r * 100000)) / 1000
-data$prec_minus_et <- data$precipitation - data$evapotranspiration
+data$vwc_L <- ((data$vwc / data$dbovendry_r) * (data$dbovendry_r * 50000)) / 1000
+data$fc_L <- ((data$fc / data$dbovendry_r) * (data$dbovendry_r * 50000)) / 1000
+data$potential_water <- data$precipitation - data$evapotranspiration + data$vwc_L
 data$total_porosity <- 1 - (data$dbovendry_r / 2.65)
-data$litter_to_saturation <- ((data$total_porosity / data$dbovendry_r) * (data$dbovendry_r * 100000)) / 1000
+data$litter_to_saturation <- ((data$total_porosity / data$dbovendry_r) * (data$dbovendry_r * 50000)) / 1000
+data$potential_water_minus_runoff <- ifelse(
+  data$potential_water > data$litter_to_saturation, 
+  data$litter_to_saturation, 
+  data$potential_water
+)
+data$potential_water_minus_runoff <- ifelse(data$potential_water_minus_runoff < 0, 0, data$potential_water_minus_runoff) 
+data$percolation <- ifelse(
+  data$potential_water_minus_runoff < 0, 
+  0, 
+  data$potential_water - data$potential_water_minus_runoff - data$fc_L
+)
+data$volumetric_flux <- 0.05 / 86400
+data$pore_velocity <- data$volumetric_flux / data$fc
+data$ke <- 0.5 * data$potential_water_minus_runoff * (data$pore_velocity ^ 2)
+
+# summary(data)
+# hist(data$ke)
+# hist(data$pore_velocity)
+# hist(data$volumetric_flux)
+# hist(data$potential_water_minus_runoff)
+# hist(data$percolation)
+# hist(data$vwc)
+# hist(data$vwc_L)
+# hist(data$evapotranspiration)
+# hist(data$precipitation)
+# hist(data$total_porosity)
+# hist(data$litter_to_saturation)
+# hist(data$stock)
+# hist(data$dbovendry_r)
 
 # write to disk
 write.csv(data, "output/final_data.csv", row.names = F)
-
-summary(data)
-hist(data$vwc)
-hist(data$vwc_L)
-hist(data$evapotranspiration)
-hist(data$precipitation)
-hist(data$prec_minus_et)
-hist(data$total_porosity)
-hist(data$litter_to_saturation)
-hist(data$stock)
-hist(data$dbovendry_r)
-
-# aggregate
-data_agg <- data %>% 
-  group_by(id, county) %>% 
-  summarise(
-    texture = first(texture), 
-    min_precipitation = min(precipitation),
-    stock = first(stock)
-  )
-
-data_agg
-data_agg$texture <- factor(data_agg$texture)
-mod <- lm(log(stock) ~ county + texture + min_precipitation, data = data_agg)
-summary(mod)
-plot(data_agg$stock, mod$fitted.values)
-plot(log(data_agg$stock), mod$fitted.values)
-cor(log(data_agg$stock), mod$fitted.values)
