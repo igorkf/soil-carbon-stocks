21ce73d2beaeea807fb800c3b5d6dabf049f6a65
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2023-10-24 11:21:54 -0500

    fix ssurgo to download ar and ca

--- src/02-download-ssurgo.R
+++ src/02-download-ssurgo.R
@@ -7,43 +7,56 @@ library(terra)
 # load sol util functions
 source("src/soil_utils.R")
 
-df_points <- read.csv("output/points.csv")
-
-cat("# points to extract SSURGO data:", nrow(df_points), "\n")
-socs <- data.frame()
-horizons <- data.frame()
-for (i in 1:nrow(df_points)) {
-  try({
-    p <- vect(df_points[i, c("lon", "lat")], crs = "epsg:4326")
-    mu <- SDA_spatialQuery(p, what = 'mupolygon', geomIntersection = T)
-    s <- processSSURGO(mu$mukey, .top = 0, .bottom = 20)
-    # soc
-    soc <- estimateStock(s)
-    soc$id <- df_points[i, "id"]
-    socs <- rbind(socs, soc)
-    # horizons
-    hz <- horizons(s)
-    hz$mukey <- mu$mukey
-    hz$id <- df_points[i, "id"]
-    horizons <- rbind(horizons, hz)
-  })
+extract_ssurgo <- function(data) {
+  cat("# points to extract SSURGO data:", nrow(data), "\n")
+  socs <- data.frame()
+  horizons <- data.frame()
+  for (i in 1:nrow(data)) {
+    try({
+      p <- vect(data[i, c("lon", "lat")], crs = "epsg:4326")
+      mu <- SDA_spatialQuery(p, what = 'mupolygon', geomIntersection = T)
+      s <- processSSURGO(mu$mukey, .top = 0, .bottom = 20)
+      # soc
+      soc <- estimateStock(s)
+      soc$id <- data[i, "id"]
+      socs <- rbind(socs, soc)
+      # horizons
+      hz <- aqp::horizons(s)
+      hz$mukey <- mu$mukey
+      hz$id <- data[i, "id"]
+      horizons <- rbind(horizons, hz)
+    })
+  }
+  return(list(socs = socs, horizons = horizons))
 }
 
-# remove duplicated points because some sampled points lie in the same mukey
-socs <- socs[!duplicated(socs$mukey), ]
-rownames(socs) <- NULL
-socs$id <- as.integer(socs$id)
-horizons$hzID_mukey <- paste0(horizons$hzID, "_", horizons$mukey)
-horizons <- horizons[!duplicated(horizons$hzID_mukey), ]
-rownames(horizons) <- NULL
-horizons$hzID_mukey <- NULL
+clean_ssurgo <- function(socs, horizons) {
+  # remove duplicated points because some sampled points lie in the same mukey
+  socs <- socs[!duplicated(socs$mukey), ]
+  rownames(socs) <- NULL
+  socs$id <- as.integer(socs$id)
+  horizons$hzID_mukey <- paste0(horizons$hzID, "_", horizons$mukey)
+  horizons <- horizons[!duplicated(horizons$hzID_mukey), ]
+  rownames(horizons) <- NULL
+  horizons$hzID_mukey <- NULL
+  return(list(socs = socs, horizons = horizons))
+}
 
-# summaries
-cat("summary SOC:\n")
-summary(socs)
-cat("summary horizons\n:")
-summary(horizons)
 
-# write datasets
-write.csv(socs, "output/soc.csv", row.names = F)
-write.csv(horizons, "output/horizons.csv", row.names = F)
+# ARKANSAS
+points_ar <- read.csv("output/Arkansas/points.csv")
+ssurgo_ar <- extract_ssurgo(points_ar)
+ssurgo_ar <- clean_ssurgo(ssurgo_ar$socs, ssurgo_ar$horizons)
+summary(ssurgo_ar$socs)
+summary(ssurgo_ar$horizons)
+write.csv(ssurgo_ar$socs, "output/Arkansas/socs.csv", row.names = F)
+write.csv(ssurgo_ar$horizons, "output/Arkansas/horizons.csv", row.names = F)
+
+# CALIFORNIA
+points_ca <- read.csv("output/California/points.csv")
+ssurgo_ca <- extract_ssurgo(points_ca)
+ssurgo_ca <- clean_ssurgo(ssurgo_ca$socs, ssurgo_ca$horizons)
+summary(ssurgo_ca$socs)
+summary(ssurgo_ca$horizons)
+write.csv(ssurgo_ca$socs, "output/California/socs.csv", row.names = F)
+write.csv(ssurgo_ca$horizons, "output/California/horizons.csv", row.names = F)
