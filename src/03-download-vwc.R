dc3ff4ab12175156e03d5465686b7877f77fecab
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2023-10-24 11:41:49 -0500

    fix vwc scripts to account for ar and ca

--- src/03-download-vwc.R
+++ src/03-download-vwc.R
@@ -8,28 +8,40 @@
 # top soil (top 5 cm)
 # sub soil (top 100 cm)
 
+build_urls <- function(data, fips) {
+  # build URLs
+  data$layer <- paste0("SMAP-9KM-DAILY-TOP_", data$date, "_AVERAGE")
+  data$url <- paste0(
+    "https://cloud.csiss.gmu.edu/smap_service?service=WPS&version=1.0.0&request=Execute&identifier=GetFileByFips&DataInputs=layer=",
+    data$layer, ';fips=', fips
+  )
+  return(data)
+}
+
+extract_vwc <- function(data, state) {
+  for (i in 1:nrow(data)) {
+    destfile <- paste0(file.path("output/", state, "/", data$layer[[i]]), ".tif")
+    lines <- readLines(data$url[[i]], warn = F)
+    lines <- strsplit(lines, "(<|>)")
+    lines <- unlist(lines)
+    url <- grep("https://.*.tif", lines, value = T)
+    if (!identical(url, character(0))) {
+      download.file(url, destfile, method = "wget", extra = "--no-check-certificate", quiet = T)
+    }
+    cat("Day:", data$date[[i]], "\n")
+  }
+}
+
+
 start <- as.Date("2023-01-01")
 end <- as.Date("2023-08-31")
-df <- data.frame(date = seq(start, end, 1))
-df$date <- gsub("-", ".", df$date)
+data <- data.frame(date = seq(start, end, 1))
+data$date <- gsub("-", ".", data$date)
 
-# build URLs
-stateFIPS <- "05"  # Arkansas
-df$layer <- paste0("SMAP-9KM-DAILY-TOP_", df$date, "_AVERAGE")
-df$url <- paste0(
-  "https://cloud.csiss.gmu.edu/smap_service?service=WPS&version=1.0.0&request=Execute&identifier=GetFileByFips&DataInputs=layer=",
-  df$layer, ';fips=', stateFIPS
-)
+# ARKANSAS
+urls_ar <- build_urls(data, fips = "05")
+extract_vwc(urls_ar, "Arkansas")
 
-# download all images
-for (i in 1:nrow(df)) {
-  destfile <- paste0(file.path("output", df$layer[[i]]), ".tif")
-  lines <- readLines(df$url[[i]], warn = F)
-  lines <- strsplit(lines, "(<|>)")
-  lines <- unlist(lines)
-  url <- grep("https://.*.tif", lines, value = T)
-  if (!identical(url, character(0))) {
-    download.file(url, destfile, method = "wget", extra = "--no-check-certificate", quiet = T)
-  }
-  cat("Day:", df$date[[i]], "\n")
-}
+# CALIFORNIA
+urls_ca <- build_urls(data, fips = "06")
+extract_vwc(urls_ca, "California")
--- src/04-preprocess-vwc.R
+++ src/04-preprocess-vwc.R
@@ -7,41 +7,53 @@ library(ggplot2)
 library(sf)
 library(terra)
 
-# read all rasters
-rasters <- list.files(path = "output", pattern = ".tif$", all.files = T, full.names = T)
-
-# read points of interest and intersect with VWC
-df_points <- read.csv("output/points.csv")
-ps <- vect(df_points[, c("lon", "lat")], crs = "epsg:4326")
-
-# read raster, project, and intersect with points of interest
-df_vwc <- data.frame()
-for (path in rasters) {
-  r <- rast(path)
-  r <- project(r, "epsg:4326")
-  df_temp <- extract(r, ps)
-  colnames(df_temp) <- c("id", "vwc")
-  df_temp$path <- path
-  df_temp <- cbind(df_points, df_temp[, c("vwc", "path")])  # bind points dataset
-  df_vwc <- rbind(df_vwc, df_temp)
-  cat("raster:", path, "\n")
+intersect_points <- function(data, path_rasters) {
+  ps <- vect(data[, c("lon", "lat")], crs = "epsg:4326")
+  rasters <- list.files(path = path_rasters, pattern = ".tif$", all.files = T, full.names = T)
+  df_vwc <- data.frame()
+  for (path in rasters) {
+    r <- rast(path)
+    r <- project(r, "epsg:4326")
+    df_temp <- extract(r, ps)
+    colnames(df_temp) <- c("id", "vwc")
+    df_temp$path <- path
+    df_temp <- cbind(data, df_temp[, c("vwc", "path")])  # bind points dataset
+    df_vwc <- rbind(df_vwc, df_temp)
+    cat("raster:", path, "\n")
+  }
+  return(df_vwc)
 }
 
-# order by point id
-df_vwc <- df_vwc[order(df_vwc$id), ]
-rownames(df_vwc) <- NULL
+prepare_points <- function(data) {
+  data <- data[order(data$id), ]
+  rownames(data) <- NULL
+  # create index from path
+  data$day <- sequence(tabulate(data$id))
+  data$id <- factor(data$id)
+  return(data)
+}
 
-# create index from path
-df_vwc$day <- sequence(tabulate(df_vwc$id))
-df_vwc$id <- factor(df_vwc$id)
 
-# plot VWC time series for each county
-ggplot(df_vwc, aes(x = day, y = vwc)) +
+# ARKANSAS
+points_ar <- read.csv("output/Arkansas/points.csv")
+vwc_ar <- intersect_points(points_ar, "output/Arkansas")
+vwc_ar <- prepare_points(vwc_ar)
+ggplot(vwc_ar, aes(x = day, y = vwc)) +
   geom_line(aes(group = id), linewidth = 0.2, alpha = 0.5) +
   facet_wrap(~county) + 
-  labs(x = 'Week number', y = 'VWC', 
+  labs(x = 'Day of the Year', y = 'VWC', 
        title = 'Volumetric Water Content of 20 sampled points from Jan to Set (2023)') +
   theme_bw()
+write.csv(vwc_ar, "output/Arkansas/vwc.csv", row.names = F)
 
-# write vwc to file
-write.csv(df_vwc, "output/vwc.csv", row.names = F)
+# CALIFORNIA
+points_ca <- read.csv("output/California/points.csv")
+vwc_ca <- intersect_points(points_ca, "output/California")
+vwc_ca <- prepare_points(vwc_ca)
+ggplot(vwc_ca, aes(x = day, y = vwc)) +
+  geom_line(aes(group = id), linewidth = 0.2, alpha = 0.5) +
+  facet_wrap(~county) + 
+  labs(x = 'Day of the Year', y = 'VWC', 
+       title = 'Volumetric Water Content of 20 sampled points from Jan to Set (2023)') +
+  theme_bw()
+write.csv(vwc_ca, "output/California/vwc.csv", row.names = F)
